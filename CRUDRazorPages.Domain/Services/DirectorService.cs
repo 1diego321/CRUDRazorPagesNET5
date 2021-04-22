1dfebfbdc7e310b6a7b08ee71653c8c8aa96d968
Luis Diego Solis Camacho <1diego321@gmail.com>
2021-04-22 14:25:13 -0600

    Director service

---
+++ CRUDRazorPages.Domain/Services/DirectorService.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using CRUDRazorPages.DataAccess.Entities;
+using CRUDRazorPages.DataAccess.Repositories.IRepositories;
+using CRUDRazorPages.Domain.Models;
+using CRUDRazorPages.Domain.Services.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDRazorPages.Domain.Services
+{
+    public class DirectorService : IDirectorService
+    {
+        private readonly IDirectorRepository _repository;
+        private readonly IMapper _mapper;
+
+        public DirectorService(IDirectorRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<Director>> GetAll()
+        {
+            return (await _repository.GetAll()).Select(x => _mapper.Map<Director>(x)).ToList();
+        }
+
+        public async Task<Director> GetById(int id)
+        {
+            return _mapper.Map<Director>(await _repository.GetById(id));
+        }
+
+        public async Task<int> Add(Director model)
+        {
+            return await _repository.Add(_mapper.Map<DirectorEntity>(model));
+        }
+
+        public async Task<int> Update(Director model)
+        {
+            return await _repository.Update(_mapper.Map<DirectorEntity>(model));
+        }
+
+        public async Task<int> Delete(int id)
+        {
+            return await _repository.Delete(id);
+        }
+    }
+}
